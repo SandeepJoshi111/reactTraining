01f3ce18c39528f52da537d7188b971e2dc46769
SandeepJoshi <joshisandip111@gmail.com>
2022-11-12 09:18:26 +0545

    Deleted tic tac toe files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Game from './Components/Game';
+
 
 function App() {
   return (
     <div className="App">
-      <Game/>
+      
     </div>
   );
 }
